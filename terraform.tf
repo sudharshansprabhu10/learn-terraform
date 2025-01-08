56b17ec05b27970a6d8595e233068dd88d19053a
Sudharshan S Prabhu <sudharshansprabhu@gmail.com>
2025-01-08 14:50:16 +0530

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "sudharshansprabhu-org"
 
     workspaces {
       name = "learn-terraform"
     }
   }
+  */
 
   required_providers {
     aws = {
